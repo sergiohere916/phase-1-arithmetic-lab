e474ca435102f4145bebb77c2c8c39c59a061fe9
Sergio Heredia <sergioheredia1996@gmail.com>
2023-07-31 21:11:20 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 34;
+let num4 = 5;
+let num5 = 1;
+let num6 = 7;
+let num7 = 20; 
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 5 + 1);
+const mod = num3 % num4;
+const max = Math.max(num5, num6, num7);
